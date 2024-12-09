40fca35020aebb62e27721260b9d4c402bba5cc2
Jack Stoller <me@jackstoller.com>
2024-12-08 21:39:52 -0500

    Fixed visual arrow bug in image carousels and added favicon

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,7 +8,11 @@ export default createHandler(() => (
         <head>
           <meta charset='utf-8' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <link rel='icon' href='/favicon.ico' />
+          <link
+            rel='icon'
+            type='image/png'
+            href='/images/wave.png'
+          />
           {assets}
         </head>
         <body>
